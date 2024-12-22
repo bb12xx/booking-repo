f6a037017ec8e90233e10bf2bed93e86f9e806bf
bb12xx <bristibhuyan@yahoo.com>
2024-12-22 17:51:42 +0530

    commit message 2

---
+++ src/main/java/com/example/booking/bookingautomation/BookingautomationApplication.java
@@ -0,0 +1,29 @@
+package com.example.booking.bookingautomation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingautomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingautomationApplication.class, args);
+		System.out.println("Main branch");
+		System.out.println("Main branch");
+		System.out.println("Main branch");
+		System.out.println("Main branch");
+		System.out.println("Main branch");
+		System.out.println("Main branch");
+		System.out.println("Main branch");
+		System.out.println("Main branch");
+		System.out.println("Main branch");
+		System.out.println("Main branch");
+		System.out.println("Main branch");
+		System.out.println("Main branch");
+		System.out.println("Main branch");
+		System.out.println("Main branch");
+		System.out.println("Main branch");
+		System.out.println("Main branch");
+	}
+
+}
