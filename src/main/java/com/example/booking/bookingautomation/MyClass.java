816475cbed7c3ab869b5bae16a3fa63b8aabd919
bb12xx <bristibhuyan@yahoo.com>
2024-12-22 21:54:47 +0530

    hi

--- src/main/java/com/example/booking/bookingautomation/MyClass.java
+++ src/main/java/com/example/booking/bookingautomation/MyClass.java
@@ -10,4 +10,15 @@ public class MyClass {
     {
         return "Hello1234";
     }
+
+    String getString2()
+    {
+        return "Hello1234";
+    }
+
+    String getString3()
+    {
+        return "Hello12345";
+    }
 }
+
