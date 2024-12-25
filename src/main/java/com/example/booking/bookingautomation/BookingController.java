08480ab05f34f1b719abe0591a34a2f6a7153f90
bb12xx <bristibhuyan@yahoo.com>
2024-12-25 19:25:05 +0530

    commit child 1-8

--- src/main/java/com/example/booking/bookingautomation/BookingController.java
+++ src/main/java/com/example/booking/bookingautomation/BookingController.java
@@ -7,51 +7,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/booking")
 public class BookingController {
-    @GetMapping("/getdetails")
-    public String getDetails()
+    @GetMapping("/getdetailschild")
+    public String getdetailschild()
     {
-        return "Hi main";
-    }
-    @GetMapping("/getdetails1")
-    public String getDetails1()
-    {
-        return "Hi child2";
-    }
-    @GetMapping("/getdetails2")
-    public String getDetails2()
-    {
-        return "Hi child1";
+        return "Hi child";
     }
 
-    @GetMapping("/getdetails3")
-    public String getDetails3()
-    {
-        return "Hi child3";
-    }
-
-    @GetMapping("/getdetails4")
-    public String getDetails4()
-    {
-        return "Hi child4";
-    }
-
-    @GetMapping("/getdetails10")
-    public String getDetails10()
-    {
-        return "Hi child10";
-    }
-
-    @GetMapping("/getdetails11")
-    public String getDetails11()
-    {
-        return "Hi child11";
-    }
-
-    @GetMapping("/getdetails41")
-    public String getDetails41()
-    {
-        return "Hi child41";
-    }
 
     @GetMapping("/getdetailsChild1")
     public String getDetailsChild1()
