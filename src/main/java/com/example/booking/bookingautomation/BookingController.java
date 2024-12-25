eaad40e89b0050b901fcfbd2b9eba04902e2a98a
bb12xx <bristibhuyan@yahoo.com>
2024-12-25 19:24:40 +0530

    commit main-4

--- src/main/java/com/example/booking/bookingautomation/BookingController.java
+++ src/main/java/com/example/booking/bookingautomation/BookingController.java
@@ -7,97 +7,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/booking")
 public class BookingController {
-    @GetMapping("/getdetails")
-    public String getDetails()
+    @GetMapping("/getdetailsmain")
+    public String getDetailsmain()
     {
         return "Hi main";
     }
-    @GetMapping("/getdetails1")
-    public String getDetails1()
+
+    @GetMapping("/getdetailsmain1")
+    public String getDetailsmain1()
     {
-        return "Hi child2";
-    }
-    @GetMapping("/getdetails2")
-    public String getDetails2()
-    {
-        return "Hi child1";
+        return "Hi main1";
     }
 
-    @GetMapping("/getdetails3")
-    public String getDetails3()
-    {
-        return "Hi child3";
-    }
-
-    @GetMapping("/getdetails4")
-    public String getDetails4()
-    {
-        return "Hi child4";
-    }
-
-    @GetMapping("/getdetails7")
-    public String getDetails7()
-    {
-        return "Hi child7";
-    }
-
-    @GetMapping("/getdetails8")
-    public String getDetails8()
-    {
-        return "Hi child8";
-    }
-
-    @GetMapping("/getdetails10")
-    public String getDetails10()
-    {
-        return "Hi child10";
-    }
-
-    @GetMapping("/getdetails11")
-    public String getDetails11()
-    {
-        return "Hi child11";
-    }
-
-    @GetMapping("/getdetails20")
-    public String getDetails20()
-    {
-        return "Hi child20";
-    }
-
-    @GetMapping("/getdetails21")
-    public String getDetails21()
-    {
-        return "Hi child21";
-    }
-
-    @GetMapping("/getdetails31")
-    public String getDetails31()
-    {
-        return "Hi child31";
-    }
-
-    @GetMapping("/getdetails91")
-    public String getDetails91()
-    {
-        return "Hi child91";
-    }
-
-    @GetMapping("/getdetails41")
-    public String getDetails41()
-    {
-        return "Hi child41";
-    }
-
-    @GetMapping("/getdetailsChild1")
-    public String getDetailsChild1()
-    {
-        return "Hi childChild1";
-    }
-
-    @GetMapping("/getdetailsMain")
-    public String getDetailsMain()
-    {
-        return "Hi childMain";
-    }
 }
